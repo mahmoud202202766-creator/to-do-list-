d68dd03e7e1d2721d566f3defe291aa23b81d510
Mahmoud Elhayad <mahmoud202202766@gmail.com>
2026-06-01 17:25:58 +0300

    make addTaskModal component

--- src/CalendarCard.jsx
+++ src/CalendarCard.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+
 import AddButton from "./AddButton";
 import Header from "./Header";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 const CalendarCard = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="my-8 w-md mx-auto bg-white">
       <Header />
       <main>
         <TaskList />
-        <AddButton />
+        <AddButton isOpen={isOpen} setIsOpen={setIsOpen} />
       </main>
+      <AddTaskModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </div>
   );
 };
