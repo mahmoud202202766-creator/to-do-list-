e4d6daf84ad72cc92dafb0030d8afe0f10b87fc6
Mahmoud Elhayad <mahmoud202202766@gmail.com>
2026-06-01 22:46:20 +0300

    persist tasks to localStorage

--- src/CalendarCard.jsx
+++ src/CalendarCard.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import AddButton from "./AddButton";
 import Header from "./Header";
 import TaskList from "./TaskList";
 import AddTaskModal from "./AddTaskModal";
 const CalendarCard = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
+
   const [search, setSearch] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   const onAdd = (task) => {
     setTasks([...tasks, { ...task, id: Date.now() }]);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
 
   const onDelete = (id) => {
